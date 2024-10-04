df611c3eb1c0ba28939651249e69efa895c2f1fc
saeed <saeed.raesi2020@gmail.com>
2024-10-04 20:36:21 +0330

    [UPD] migration 17 to 18 remove l10n_ir_account and persian_calendar (#1)

--- l10n_ir_fonts/__init__.py
+++ l10n_ir_fonts/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2024-Today: Odoo Community Iran
 # @author: Odoo Community Iran (https://odoo-community.ir/
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
