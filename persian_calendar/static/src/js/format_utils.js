277791b2c46b4d3fa2dadf30d531d0d9669803af
fadoo5 <yhyh1377@gmail.com>
2025-03-04 15:00:29 +0330

    [IMP] revert js striings

--- persian_calendar/static/src/js/format_utils.js
+++ persian_calendar/static/src/js/format_utils.js
@@ -1,6 +1,3 @@
-// Copyright (C) 2024-Today: Odoo Community Iran
-// @author: Odoo Community Iran (https://odoo-community.ir/
-// License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 /** @odoo-module */
 
 import { _t } from "@web/core/l10n/translation";
