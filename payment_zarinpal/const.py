66ac0b8fd1df66aaee73dcfd2885ef69ae9dd0f7
Ali Sharifi <75910186+ali997-code@users.noreply.github.com>
2025-03-04 15:26:07 +0330

    [ADD] add payment_zarinpal version 18 (#13)

---
+++ payment_zarinpal/const.py
@@ -0,0 +1,5 @@
+# Copyright (C) 2024-Today: Odoo Community Iran
+# @author: Odoo Community Iran (https://odoo-community.ir/
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# ISO 4217 codes of currencies supported by PayPal
+SUPPORTED_CURRENCIES = ('IRR')
