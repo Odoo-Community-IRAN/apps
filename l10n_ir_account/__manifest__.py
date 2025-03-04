5f2941e121c2c55a19ece2b36f1066fa3bf9bac7
Ali Sharifi <75910186+ali997-code@users.noreply.github.com>
2025-03-04 15:25:50 +0330

    [ADD] add l10n_ir_account module version 18 (#14)

---
+++ l10n_ir_account/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2024-Today: Odoo Community Iran
+# @author: Odoo Community Iran (https://odoo-community.ir/
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Iran - Accounting",
+    "summary": """Iran accounting chart and localization.""",
+    'author': "Odoo Community Iran",
+    'website': "https://odoo-community.ir/",
+    "category": "Accounting/Localizations/Account Charts",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "countries": ["ir"],
+    "depends": ["account"],
+    "data": [
+        "data/res_currency_data.xml",
+        "data/res.bank.csv",
+    ],
+}
