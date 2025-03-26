26adbeed13e79920d55b5864f5fd39db5c232422
fadoo5 <yhyh1377@gmail.com>
2025-03-26 13:59:08 +0330

    [IMP] screenshots replaaced

--- l10n_ir_account_reports/__manifest__.py
+++ l10n_ir_account_reports/__manifest__.py
@@ -7,7 +7,7 @@
     "summary": """Odoo Iran Print""",
     "description": """Odoo Iran Print""",
     "category": "Accounting/Localizations/Reporting",
-    "version": "16.0.1.0.0",
+    "version": "1.0.1",
     "author": "Odoo Community Iran",
     "website": "https://odoo-community.ir",
     "license": "AGPL-3",
