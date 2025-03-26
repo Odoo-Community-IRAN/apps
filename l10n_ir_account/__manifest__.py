923d2adcb8d7f0d27c376d62e78844bc6b3f8a78
fadoo5 <yhyh1377@gmail.com>
2025-03-26 13:11:14 +0330

    [IMP] improved manifest translation

--- l10n_ir_account/__manifest__.py
+++ l10n_ir_account/__manifest__.py
@@ -4,10 +4,11 @@
 {
     "name": "Iran - Accounting",
     "summary": """Iran accounting chart and localization.""",
+    "description": """Iran accounting chart and localization.""",
     'author': "Odoo Community Iran",
     'website': "https://odoo-community.ir/",
     "category": "Accounting/Localizations/Account Charts",
-    "version": "18.0.1.0.0",
+    "version": "1.0.0",
     "license": "AGPL-3",
     "countries": ["ir"],
     "depends": ["account"],
