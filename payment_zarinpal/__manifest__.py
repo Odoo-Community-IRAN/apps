bb644a5e6b5c1e36c0f5180b91b302dfa471af25
yhyh1377 <86868118+yhyh1377@users.noreply.github.com>
2025-03-26 14:32:49 +0330

    Dev zarinpal 17 (#16)
    * [ADD] add payment_zarinpal version 17
    
    * [IMP] added new translation
    
    * [FIX] install error fixed
    
    * [IMP] updated version
    
    ---------
    
    Co-authored-by: Ali Sharifi <alisharifi997as@gmail.com>

--- payment_zarinpal/__manifest__.py
+++ payment_zarinpal/__manifest__.py
@@ -8,7 +8,7 @@
     "author": "Odoo Community Iran",
     "website": "https://odoo-community.ir/",
     "category": "Accounting/Payment Acquirers",
-    "version": "17.0.1.0.0",
+    "version": "2.0.0",
     "license": "AGPL-3",
     "depends": ["base", "account", "payment"],
     "data": [
