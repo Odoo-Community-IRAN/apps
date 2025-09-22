5dbb756c2c483f0c517883ee2bc9fe6fd20d9ecb
Wahab Safi <wahab.safi3@gmail.com>
2025-09-22 21:04:34 +0330

    [INIT] Initial commit for version 19.0

--- payment_zarinpal/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (C) 2024-Today: Odoo Community Iran
-# @author: Odoo Community Iran (https://odoo-community.ir/
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from . import account_payment_method
-from . import payment_acquirer
-from . import payment_transaction
